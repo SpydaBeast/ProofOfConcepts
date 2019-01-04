cbe4a37de48a51ff6022ee6012d0c2c68442fd5f
Brien <work.brien@gmail.com>
2019-01-04 14:39:52 -0800

    adding autotrack

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ const app = express();
 app.get('/', (req,res) =>{ 
      res.sendFile(path.join(__dirname+'/index.html'));
 });
+app.get('/ga', (req,res) =>{ 
+     res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.get('/email', (req,res) =>{ 
+     res.sendFile(path.join(__dirname+'/index.html'));
+});
 app.use(express.static(__dirname));
 
 if (module === require.main) {
